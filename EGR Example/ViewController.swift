6778e7fbe385ba86308052c02bde9647a932e703
Mina Yousry <mina.yousry.samir@gmail.com>
2025-09-14 15:44:15 +0300

    Initial Commit

---
+++ EGR Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EGR Example
+//
+//  Created by Mina Yousry on 14/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
